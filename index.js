414648a8c79e155f88e12e422efa1e09a97fd655
abitshrestha <141226473+abitshrestha@users.noreply.github.com>
2023-09-07 19:22:15 +0545

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const currencyRouter=require('./routes/currency');
 const PORT=process.env.PORT;
 
 app.use(cors());
+app.get('/',(req,res)=>{
+    res.setHeader("Access-Control-Allow-Credentials","true");
+    res.send("API is running");
+});
 app.use('/getConverted',currencyRouter);
 app.listen(PORT,()=>{
   console.log(`Server is listening on port http://localhost:${PORT}`);
